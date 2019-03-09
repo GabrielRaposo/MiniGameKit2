44da59d4c860f6694a009c5af34a947e160c16b3
GabrielRaposo <gabriel.s.raposo@gmail.com>
2019-03-09 09:04:59 -0300

    Criação da base do minigame

---
+++ Assets/Minigames/Pilhas/StackableRock.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pilhas
+{
+    public class StackableRock : MonoBehaviour
+    {
+        public bool taken;
+
+        private Transform target; 
+
+        private Rigidbody2D rb2D;
+
+        void Start()
+        {
+            rb2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void SetTarget (Transform target, bool side)
+        {
+            this.target = target;
+            taken = true;
+        }
+
+        public void ReleaseTarget()
+        {
+            if (target)
+            {
+                target = null;
+                taken = false;
+                rb2D.velocity = Vector2.zero;
+            }
+        }
+
+        void Update()
+        {
+            if (target)
+            {
+                //Vector2 movement = (target.position - transform.position);
+                //rb2D.velocity += movement;
+                transform.position = target.position;
+            }
+        }
+    }
+
+}
